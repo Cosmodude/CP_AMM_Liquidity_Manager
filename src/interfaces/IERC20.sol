c9368b4a6312e757aad26379c4fed31bf9c664a9
Cosmodude <vladislav.lenskii@gmail.com>
2025-06-22 12:27:41 +0900

    interfaces updated

--- src/interfaces/IERC20.sol
+++ src/interfaces/IERC20.sol
@@ -4,5 +4,6 @@ pragma solidity ^0.8.29;
 interface IERC20 {
     function approve(address spender, uint256 amount) external returns (bool);
     function transfer(address to, uint256 amount) external returns (bool);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
     function balanceOf(address account) external view returns (uint256);
 } 
